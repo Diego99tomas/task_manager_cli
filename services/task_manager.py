6ee195af6cce4e75b85b7c7d30081ad4c957ee0d
DiegoTR <diego99tomas@gmail.com>
2026-02-20 17:25:08 -0500

    refactor: returm same type and code cleanup

--- services/task_manager.py
+++ services/task_manager.py
@@ -10,8 +10,7 @@ class TaskManager:
     def add_task(self,title:str,description:str, priority:str):
             new_task=Task(title,description,priority)
             self.list_of_tasks.append(new_task)
-            added_task=self.my_storage.save_task(self.list_of_tasks)
-            return added_task 
+            self.my_storage.save_task(self.list_of_tasks)   
             
            
     def show_all_tasks(self):
@@ -21,9 +20,9 @@ class TaskManager:
     def task_completed(self,pos:int):
         if pos<0 or pos>len(self.list_of_tasks):
             return
-        
         self.list_of_tasks[pos-1].status="completed"
         self.my_storage.save_task(self.list_of_tasks)
+
     
     def filtered_task_pending_or_completed(self,status:str):
         filtered_tasks_list=[task for task in self.list_of_tasks if task.status==status]
@@ -32,12 +31,20 @@ class TaskManager:
 
     def general_summary(self):
         if not self.list_of_tasks:
-            return 
-        completed=[task_completed for task_completed in self.list_of_tasks if task_completed.status=="completed"] 
-        pending=[task_pending for task_pending in self.list_of_tasks if task_pending.status=="pending"] 
-        rate=(len(completed)*100)/len(self.list_of_tasks)
+            return [0,0,0.0]
         
-        return len(completed),len(pending),rate
+        completed=[] 
+        pending=[] 
+        
+        for task_status in self.list_of_tasks:
+            if task_status.status == "completed":
+                completed.append(task_status)
+            else:
+                pending.append(task_status)        
+        
+        rate=(len(completed)*100)/len(self.list_of_tasks)
+        summary=[len(completed),len(pending),rate]
+        return summary
             
     
     def tasks_for_priority(self):
