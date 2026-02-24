ea8970045f636c430c2d4e8ed8a889b84872e4ea
DiegoTR <diego99tomas@gmail.com>
2026-02-24 15:59:28 -0500

    refactor: enum class for priority and status

--- models/task.py
+++ models/task.py
@@ -1,27 +1,33 @@
 from datetime import date
+from enum import Enum
 import uuid
 
+class Status(str,Enum):
+    PENDING='pending'
+    COMPLETED='completed'
+
+class Priority(str,Enum):
+    HIGH='high'
+    MEIDUM='medium'
+    LOW='low'
 
 class Task:
-    def __init__(self,title:str,description:str,priority:str,id=None,created_at=None,status="pending"):
-        priority_options=["high","medium","low"]
-        status_options=["completed","pending"]
+    def __init__(self,title:str,description:str,priority:Priority,status=Status.PENDING,id=None,created_at=None): 
         
         self.id=id if id else uuid.uuid4().__str__()
         self.title=title
         self.description=description
         
-        if priority not in priority_options:
-            raise ValueError(f"Invalid option. Choice {priority_options} " )
+        if not isinstance(priority,Priority):
+            raise ValueError(f"Invalid option ." )
         self.priority=priority
 
-        if status not in status_options:
-            raise ValueError(f"Invalid option. Choice {status_options} " )
+        if not isinstance(status,Status):
+            raise ValueError(f"Invalid option." )
         self.status=status
         
         self.created_at=created_at if created_at else date.today().__str__()
 
  
     def __str__(self):
-        return f"{self.id} | Task: {self.title} | Description: {self.description} | Priority: {self.priority} | Status: {self.status} | Date created: {self.created_at} "
-
+        return f"{self.id} | Task: {self.title} | Description: {self.description} | Priority: {self.priority.value} | Status: {self.status.value} | Date created: {self.created_at} "
