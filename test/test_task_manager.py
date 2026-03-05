39e291466ef771d4794e1bfbdcf37a07f150edcb
DiegoTR <diego99tomas@gmail.com>
2026-03-05 11:58:10 -0500

    test: testing delete task function

--- test/test_task_manager.py
+++ test/test_task_manager.py
@@ -1,51 +1,66 @@
 from services.task_manager import TaskManager
 from models.task import Status
+from core.custom_exceptions import TaskNotFoundError
 
 import pytest
 
 class FakeStorage():
     def __init__(self):
        self.save_to_do_list=[]
-       self.was_saved=False
+       self.save_calls=0
 
     def loading_tasks(self)->list:
         return self.save_to_do_list
     
     def save_task(self,new_list):
         self.save_to_do_list=new_list
-        self.was_saved=True
+        self.save_calls+=1
         
 
 @pytest.fixture
-def fake_storage():
+def task_manager():
     fake_storage=FakeStorage()
     task_manager=TaskManager(fake_storage)
     return task_manager
 
-#only if add_task return new task
-def test_if_added_task(fake_storage):
-    created_task=fake_storage.add_task("Title","Descr","high")
+
+def test_add_task_creates_task_with_default_status(task_manager):
+    created_task=task_manager.add_task("Title","Descr","high")
 
     assert created_task.title=="Title"
     assert created_task.description=="Descr"
     assert created_task.status.value==Status.PENDING
 
 
-def test_add_task_calls_storage(fake_storage):
-    fake_storage.add_task("Title","Descr","high")
-    assert fake_storage.my_storage.was_saved is True
+def test_add_task_calls_storage(task_manager):
+    task_manager.add_task("Title","Descr","high")
+    assert task_manager.my_storage.save_calls == 1
 
 
-def test_add_task_with_invalid_priority(fake_storage):
+def test_add_task_with_invalid_priority(task_manager):
     with pytest.raises(ValueError):
-        fake_storage.add_task("Title","Descr","super-high")
+        task_manager.add_task("Title","Descr","super-high")
+
+def test_delete_task_raises_if_list_empty(task_manager):
+    fake_id="1234-non-existent-id"
+
+    with pytest.raises(TaskNotFoundError):
+        task_manager.delete_task(fake_id)
+
+    assert task_manager.my_storage.save_calls == 0
 
 
-# def test_task_completed(fake_storage):
-#     fake_storage.add_task("Title","Descr","high")
-#     status_completed=fake_storage.task_completed(1)
+def test_delete_task_invalid_id_does_not_modify_list(task_manager):
+    created=task_manager.add_task("Title", "Desc", "high")
+    create_id=created.id
+    cont=task_manager.my_storage.save_calls
+    initial_length = len(task_manager.tasks)
 
-#     assert status_completed.status == Status.COMPLETED
-    
+    with pytest.raises(TaskNotFoundError):
+        task_manager.delete_task("fake-id")
+
+    assert len(task_manager.tasks) == initial_length
+    assert task_manager.my_storage.save_calls == cont
+    assert create_id in task_manager.tasks
 
         
